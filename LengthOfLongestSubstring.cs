7c585551305196003f970563d7d8f1f0e51001a3
raghadshaar <raghadshaar8@gmail.com>
2026-06-25 10:42:01 +0300

    3rd problem : LengthOfLongestSubstring

---
+++ LengthOfLongestSubstring.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+
+    {
+        int length = 0;
+        int left = 0;
+        int longest = 0;
+        HashSet<char> list = new HashSet<char>();
+        for (int i = 0; i < s.Length; i++)
+        {
+
+            while (list.Contains(s[i]))
+            {
+                list.Remove(s[left]);
+                left += 1;
+
+            }
+            list.Add(s[i]);
+            length = i - left + 1;
+            if (length > longest)
+            {
+                longest = length;
+            }
+        }
+
+        return longest;
+
+    }
+}
