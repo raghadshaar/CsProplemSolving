47b73c14e63e72a66216ec1f1dc68496bfe84dc2
raghadshaar <raghadshaar8@gmail.com>
2026-06-25 10:56:47 +0300

    9th problem : Container With Most Water

---
+++ Container With Most Water.cs
@@ -0,0 +1,22 @@
+﻿public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int max = 0;
+        int left = 0;
+        int area = 0;
+        int right = height.Length - 1;
+        while (left < right)
+        {
+            area = (right - left) * Math.Min(height[left], height[right]);
+            if (area > max) { max = area; }
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return max;
+
+
+    }
+}
