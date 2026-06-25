883c6b0168497f6200aed5c2f57296cde853015e
raghadshaar <raghadshaar8@gmail.com>
2026-06-25 10:52:24 +0300

    6th problem :  Reverse Integer

---
+++ Reverse Integer.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int Reverse(int x)
+    {
+        int reversed = 0;
+
+        while (x != 0)
+        {
+            if (reversed > int.MaxValue / 10 || reversed < int.MinValue / 10)
+            {
+                reversed = 0;
+                break;
+            }
+
+            reversed = (x % 10) + reversed * 10;
+            x /= 10;
+        }
+        return reversed;
+    }
+}
