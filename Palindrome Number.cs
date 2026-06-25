1f2153507efec56001f3ec1928efb6952712a58a
raghadshaar <raghadshaar8@gmail.com>
2026-06-25 10:55:35 +0300

    8th problem : Palindrome Number

---
+++ Palindrome Number.cs
@@ -0,0 +1,29 @@
+﻿public class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        List<int> nums = new();
+        if (x < 0)
+        {
+            return false;
+        }
+        while (x != 0)
+        {
+            nums.Add(x % 10);
+            x = x / 10;
+        }
+        int right = nums.Count - 1;
+        int left = 0;
+        while (left < right)
+        {
+            if (nums[left] != nums[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+
+        }
+        return true;
+    }
+}
